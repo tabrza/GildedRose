46ba36f6da53a53cf4941b5799b4c2e770466d47
Tabish Raza <tabishraza@gmail.com>
2018-01-04 11:23:54 +0000

    passing tests for aged brie

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,3 +1,33 @@
 describe("Gilded Rose", function() {
+  var gildedRose, items;
 
+  describe("Aged brie", function(){
+    beforeEach(function(){
+      gildedRose = new Shop([ new Item("Aged Brie", 1, 9) ]);
+      items = gildedRose.updateQuality();
+    });
+
+    it("returns aged brie", function() {
+      expect(items[0].name).toEqual("Aged Brie");
+    });
+
+    it("improves quality over time", function(){
+      expect(items[0].quality).toEqual(10);
+    });
+
+    it("does not reduce quality by half if sell by date passed", function(){
+      gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(12);
+    });
+
+    it("does not have a quality level greater than 50", function(){
+      for (var i = 0; i < 50; i++){
+        gildedRose.updateQuality();
+      }
+      expect(items[0].quality).toEqual(50);
+    });
+
+  });
+
+  
 });

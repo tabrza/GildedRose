d82fdcd7b112131ce2ba371095567de52ff6f9df
Tabish Raza <tabishraza@gmail.com>
2018-01-04 11:31:32 +0000

    passing tests for sulfuras

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -29,5 +29,28 @@ describe("Gilded Rose", function() {
 
   });
 
+  describe("Sulfuras, Hand of Ragnaros", function(){
+    beforeEach(function(){
+      gildedRose = new Shop([ new Item("Sulfuras, Hand of Ragnaros", 1, 9) ]);
+      items = gildedRose.updateQuality();
+    });
+
+    it("returns Sulfuras, Hand of Ragnaros", function() {
+      expect(items[0].name).toEqual("Sulfuras, Hand of Ragnaros");
+    });
+
+    it("does not increase in quality over time", function(){
+      gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(9);
+    });
+
+    it("sellIn does not change", function(){
+      gildedRose.updateQuality();
+      gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(1);
+    });
+
+  });
+
   
 });

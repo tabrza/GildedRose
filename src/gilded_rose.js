f3375a19cac1fbfa956c243dcc7c33075305445a
Tabish Raza <tabishraza@gmail.com>
2018-01-04 17:10:14 +0000

    revised function concert

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -27,13 +27,29 @@ class Shop {
       if(this.items[i].name === 'Aged Brie' ){
           if(this.items[i].quality < 50){
             this.items[i].quality += 1;
-          } else {
-            this.items[i].quality = 50;
           }
         }
 
       if(this.items[i].name === 'Sulfuras, Hand of Ragnaros'){
-        
+
+      }
+
+      if(this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert'){
+        if(this.items[i].quality == 49){
+          this.items[i].quality += 1;
+          this.items[i].sellIn -= 1;
+        } else if(this.items[i].quality < 50 && this.items[i].sellIn > 10){
+          this.items[i].quality += 1;
+          this.items[i].sellIn -= 1;
+        } else if(this.items[i].quality < 50 && this.items[i].sellIn > 5){
+          this.items[i].quality += 2;
+          this.items[i].sellIn -= 1;
+        } else if(this.items[i].quality < 50 && this.items[i].sellIn > 0){
+          this.items[i].quality += 3;
+          this.items[i].sellIn -= 1;
+        } else {
+          this.items[i].quality = 0;
+        }
       }
 
 

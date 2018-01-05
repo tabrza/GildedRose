42677680b4b6d348af63898ced95c75e63604cda
Tabish Raza <tabishraza@gmail.com>
2018-01-05 13:07:13 +0000

    replace numbers with constants

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -6,6 +6,10 @@ class Item {
   }
 }
 
+const MINSELLBY = 0;
+const MAXQUALITY = 50;
+const MINQUALITY = 0;
+
 class Shop {
   constructor(items=[]){
     this.items = items;
@@ -14,17 +18,17 @@ class Shop {
     for (var i = 0; i < this.items.length; i++) {
 
       if(this.items[i].name === 'Elixir of the Mongoose' || this.items[i].name === '+5 Dexterity Vest'){
-        if(this.items[i].sellIn > 0 && this.items[i].quality > 0){
+        if(this.items[i].sellIn > MINSELLBY && this.items[i].quality > MINQUALITY){
           this.items[i].quality -= 1;
           this.items[i].sellIn -= 1;
-        } else if(this.items[i].sellIn <= 0 && this.items[i].quality > 0){
+        } else if(this.items[i].sellIn <= MINSELLBY && this.items[i].quality > MINQUALITY){
           this.items[i].quality -= 2;
         } else {
           this.items[i].quality = 0;
         }
       }
 
-      if(this.items[i].name === 'Aged Brie' && this.items[i].quality < 50 ){
+      if(this.items[i].name === 'Aged Brie' && this.items[i].quality < MAXQUALITY ){
           this.items[i].quality += 1;
       }
 
@@ -36,13 +40,13 @@ class Shop {
         if(this.items[i].quality == 49){
           this.items[i].quality += 1;
           this.items[i].sellIn -= 1;
-        } else if(this.items[i].quality < 50 && this.items[i].sellIn > 10){
+        } else if(this.items[i].quality < MAXQUALITY && this.items[i].sellIn > 10){
           this.items[i].quality += 1;
           this.items[i].sellIn -= 1;
-        } else if(this.items[i].quality < 50 && this.items[i].sellIn > 5){
+        } else if(this.items[i].quality < MAXQUALITY && this.items[i].sellIn > 5){
           this.items[i].quality += 2;
           this.items[i].sellIn -= 1;
-        } else if(this.items[i].quality < 50 && this.items[i].sellIn > 0){
+        } else if(this.items[i].quality < MAXQUALITY && this.items[i].sellIn > MINSELLBY){
           this.items[i].quality += 3;
           this.items[i].sellIn -= 1;
         } else {
@@ -51,10 +55,10 @@ class Shop {
       }
 
       if(this.items[i].name === 'Conjured Mana Cake'){
-        if(this.items[i].sellIn > 0 && this.items[i].quality > 0){
+        if(this.items[i].sellIn > MINSELLBY && this.items[i].quality > MINQUALITY){
           this.items[i].quality -= 2;
           this.items[i].sellIn -= 1;
-        } else if(this.items[i].sellIn <= 0 && this.items[i].quality > 0){
+        } else if(this.items[i].sellIn <= MINSELLBY && this.items[i].quality > MINQUALITY){
           this.items[i].quality -= 4;
         } else {
           this.items[i].quality = 0;

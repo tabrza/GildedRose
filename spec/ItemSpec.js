75e0d0264956bdc0bbc00549f4065e2bd066f38a
Tabish Raza <tabishraza@gmail.com>
2018-01-23 10:11:31 +0000

    item spec completed

--- spec/ItemSpec.js
+++ spec/ItemSpec.js
@@ -1,3 +1,19 @@
 describe("Item", function(){
+  beforeEach(function(){
+    item = new Item('milk', 4, 3);
+  });
 
+  describe('values', function(){
+    it('returns milk as the name', function(){
+      expect(item.name).toEqual('milk');
+    });
+
+    it('returns 4 as sellIn', function(){
+      expect(item.sellIn).toEqual(4);
+    });
+
+    it('returns 3 as quality', function(){
+      expect(item.quality).toEqual(3);
+    });
+  });
 });
